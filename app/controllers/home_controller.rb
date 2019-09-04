d01df4758635526416a2078949da6be929a248d7
Dung Luu <dung.luu07@gmail.com>
2019-09-04 08:16:17 +0700

    first display list of results

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,22 @@ class HomeController < ApplicationController
     def search
         @key = params[:search]
         # debugger 
-        @search = ImportData.where('rumi LIKE ?', "%#{@key}%")
-        render json: {result: @search.to_json}
+        @search = ImportData.where('rumi LIKE ?', "#{@key}%").limit(10)
+        
+        # h = Hash.new
+
+        @results = []
+        @search.each do |result|
+            # debugger
+            h = {}
+            h["title"] = result.rumi
+            h["description"] = result.vietnamese
+            @results << h
+            # debugger
+        end
+        # debugger
+
+        render json: {result: @results.to_json}
 
         # @search = ImportData.where(:rumi => @key)
         # debugger  
