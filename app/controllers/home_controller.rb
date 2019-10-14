4e24cb701b5deed96b87f72bfbdece0f3b679676
Dung Luu <dung.luu07@gmail.com>
2019-10-14 16:04:39 +0700

    rewrite function in home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,40 +11,32 @@ class HomeController < ApplicationController
             result["description"]["akharThrah"] = word.akharThrah
             result["description"]["source"] = word.source
             result["description"]["pronunciation"] = word.pronunciation
-            fullMeaning = []
-            word.extendeds.each do |exten|
-                meaning ={}
-                meaning["meaning"] = {}
-                meaning["meaning"]["wordClasses"] = exten.wordClasses
-                meaning["meaning"]["rumi"] = exten.rumi
-                meaning["meaning"]["akharThrah"] = exten.akharThrah
-                meaning["meaning"]["source"] = exten.source
-                meaning["meaning"]["vietnamese"] = exten.vietnamese
-                meaning["meaning"]["french"] = exten.french
-                meaning["meaning"]["english"] = exten.english
-                
-                meaning["list"] = []
-                exten.examples.each do |exam|
-                    example = {}
-                    example["rumi"] = exam.rumi
-                    example["akharThrah"] = exam.akharThrah
-                    example["source"] = exam.source
-                    example["vietnamese"] = exam.vietnamese
-                    example["french"] = exam.french
-                    example["english"] = exam.english
-                    
-                    meaning["list"] << example
-                end
-
-                fullMeaning << meaning
-            end
-
             
-            result["description"]["fullDescription"] = fullMeaning.to_json
+            build_extendeds(word)
+            
+            result["description"]["fullDescription"] = @fullMeaning.to_json
             results << result
         end
 
         render json: {result: results.to_json}
+    end
+
+    def build_extendeds(word)
+        @fullMeaning = []
+        word.extendeds.each do |exten|
+            @meaning ={}
+            @meaning["meaning"] = exten
+
+            build_examples(exten)
+            @fullMeaning << @meaning
+        end
+    end
+
+    def build_examples(exten)
+        @meaning["list"] = []
+        exten.examples.each do |exam|       
+            @meaning["list"] << exam
+        end
     end
 end
 
