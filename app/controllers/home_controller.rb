b385fa5b255d0dc9f48e2e2c0cdd246b7362ebb4
Dung Luu <dung.luu07@gmail.com>
2019-10-16 13:42:39 +0700

    dung_refactor

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 
 class HomeController < ApplicationController
   def search
@@ -6,42 +5,14 @@ class HomeController < ApplicationController
     search = Word.where('rumi LIKE ?', "#{key}%").order(:rumi).includes(:extendeds, :examples)
 
     results = []
-    search.each do |word|
-      result = word.attributes.slice('akharThrah', 'rumi')
+    search.each do |word|      
+      result = word.attributes
       result['extendeds'] = word.extendeds
-      #   result['title'] = word.rumi
-      #   result['description'] = {}
-      #   result['description']['akharThrah'] = word.akharThrah
-      #   result['description']['source'] = word.source
-      #   result['description']['pronunciation'] = word.pronunciation
-
-      fullMeaning = build_extendeds(word)
-
-      result['description']['fullDescription'] = fullMeaning.to_json
+      result['examples'] = word.examples
+      
       results << result
     end
-
     render json: { result: results.to_json }
   end
 
-  private
-
-  def build_extendeds(word)
-    fullMeaning = []
-    word.extendeds.each do |exten|
-      @meaning = {}
-      @meaning['meaning'] = exten
-
-      build_examples(exten)
-      fullMeaning << @meaning
-    end
-    fullMeaning
-  end
-
-  def build_examples(exten)
-    @meaning['list'] = []
-    exten.examples.each do |exam|
-      @meaning['list'] << exam
-    end
-  end
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -2,5 +2,4 @@ class Word < ApplicationRecord
     self.table_name = "word"
     has_many :extendeds, foreign_key: :wordId
     has_many :examples, foreign_key: :extendedId, through: :extendeds
-    # has_many :examples, foreign_key: :wordId
 end
